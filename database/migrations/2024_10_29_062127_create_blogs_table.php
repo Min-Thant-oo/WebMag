88dacc66af7acd185cc5b60b2e1e8675ba5f6fff
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-10-29 17:42:42 -0500

    created models and migration files

---
+++ database/migrations/2024_10_29_062127_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('thumbnail');
+            $table->string('image');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->foreignId('blog_status_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('published_at')->nullable();
+            $table->boolean('is_featured');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
