a6b7d7d22722aac346784ed401a5c2a21335abfd
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-02-12 21:13:01 -0600

    update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,11 @@
+<?php
+
 $providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
 ];
 
+// Conditionally add the IdeHelperServiceProvider in the local environment
 if (app()->environment('local')) {
     $providers[] = Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class;
 }
